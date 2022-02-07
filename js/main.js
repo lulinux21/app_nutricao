c34b5b09ef015dd796d0d06e9fcfac7b6dc3762e
Lucas Gomes <lulinux2109@gmail.com>
2022-02-06 21:23:45 -0300

    Removendo usuário

--- js/main.js
+++ js/main.js
@@ -14,17 +14,18 @@ for (var i = 0; i < pacientes.length; i++) {
 
   var tdImc = paciente.querySelector(".info-imc");
 
-  var pesoEhValido = true;
-  var alturaEhValida = true;
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
 
-  if (peso <= 0 || peso >= 1000) {
+  // O peso é true, entretanto para validar preciso negar
+  if (!pesoEhValido) {
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido";
     paciente.classList.add("paciente-invalido");
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!alturaEhValida) {
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
@@ -34,6 +35,24 @@ for (var i = 0; i < pacientes.length; i++) {
   if (pesoEhValido && alturaEhValida) {
     var imc = calculaImc(peso, altura);
     tdImc.textContent = imc;
+  }
+}
+
+// Validando o peso do paciente
+function validaPeso(peso) {
+  if (peso >= 0 && peso < 300) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Validando a altura do paciente
+function validaAltura(altura) {
+  if (altura >= 0.48 && altura < 3.0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
